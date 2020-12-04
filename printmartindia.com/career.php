46cda8bd1d5d782358670655152df34ad8a8de4c
shamshadali786 <73374020+shamshadali786@users.noreply.github.com>
2020-12-04 08:35:53 +0530

    Add files via upload

---
+++ printmartindia.com/career.php
@@ -0,0 +1,39 @@
+<?php include("include/header.php"); ?>
+
+<!-- -----------career header section-------------- -->
+<div id="career-menu" class="bg-dark">
+    <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+                <a class="career-logo" href="index.php">
+                    <img src="img/logo-icon/logo.png" class="img-fluid" alt="logo">
+                </a>
+            </div>
+            <div id="cv-btn">
+                <div class="button-8">
+                    <div class="eff-8"></div>
+                    <a href="#"> <strong>Send your CV at hr@printmartindia.com</strong> </a>
+                </div>
+            </div>
+        </nav>
+    </div>
+</div>
+<!-- -----------career header section-------------- -->
+
+
+<div id="service-section">
+    <div class="container">
+        <div class="service-inner">
+            <!-- <div class="heading">
+                <h1>Contact</h1>
+            </div> -->
+            <div class="service-banner">
+                <img class="img-fluid" src="img/gallery/contact.jpg" alt="contact">
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<?php include("include/footer.php"); ?>
